e757ddf0216a1ef59490c7bf72338be6eefe404f
angelscale <liangshicheng.daniel@gmail.com>
2017-03-16 22:23:54 +0800

    <infoDemo>

---
+++ daniel/infoDemo.js
@@ -0,0 +1,10 @@
+var info = {
+	workbooks = {'obesity', 'college admission'};
+	var college= {
+		filterNames = {'academic year', 'college', 'select gender'};
+		fieldNames = {'2013', '2014', 'male', 'female', 'arts and sciences', 'business', 'communication', 'education', 'engineering', 'music', 'public affairs', 'public health'};
+		academicYear = {'2013', '2014'};
+		college ={'arts and sciences', 'business', 'communication', 'education', 'engineering', 'music', 'public affairs', 'public health'};
+		gender = {'male', 'female'};
+	};
+};

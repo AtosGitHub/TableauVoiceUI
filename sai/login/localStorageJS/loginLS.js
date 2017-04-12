8309a211f15346c758e61fcd0bc827de01b85487
Saripella <rxs131930@utdallas.edu>
2017-04-11 20:22:05 -0500

    linking html pages

---
+++ sai/login/localStorageJS/loginLS.js
@@ -0,0 +1,25 @@
+function inputLocal() {
+
+    var location = document.getElementById("url").value;
+
+
+
+    window.open('../../project/blank-page.html');
+
+
+
+
+}
+
+
+function mainLocal() {
+
+    //var user = document.getElementById("userid").value;
+
+
+    window.location.pathname = "../input.html";
+
+
+
+
+}

1972d96ba528e19ecf42eeccf1897575ea73dcd5
randolph <jbrew807@gmail.com>
2017-04-19 19:38:32 -0500

    personal backup to work on substituting current SheetList with Filter object data from API

--- project_demo_v01/tableau.js
+++ project_demo_v01/tableau.js
@@ -5,7 +5,7 @@
 //----------------------------------------------------------------
 // global variables
 
-var viz, workbook, activeSheet, sheets;        
+var viz, workbook, activeSheet, sheets, views;        
 
 //----------------------------------------------------------------
 // initialize Tableau Viz
@@ -21,11 +21,15 @@ function initViz() {
         //height: "707px",
         //(optionl) instanceIdToClone: , height: ,width: ,device: ,"filterName": ,
         onFirstInteractive: function () {
+            //viz.pauseAutomaticUpdatesAsync();
             workbook = viz.getWorkbook();
             activeSheet = workbook.getActiveSheet();
             loadSheetList();
             updateSheetList();
+            views = [];
+            //saveView();
             viz.addEventListener(tableau.TableauEventName.TAB_SWITCH, updateSheetList);
+            //viz.addEventListener(tableau.TableauEventName.FILTER_CHANGE, refreshData);
         }
     };
 
@@ -52,7 +56,8 @@ function changeSheet(){
 
 function filterByName(field, filter, type) {
 
-    var sheet = viz.getWorkbook().getActiveSheet();
+    //var sheet = viz.getWorkbook().getActiveSheet();
+    sheet = activeSheet;
     if(filter.includes(";")){
         filter = filter.split(";");
     }
@@ -75,23 +80,146 @@ function applyFilter(sheet, field, filter, type){
     var fTypes = ["ALL", "REPLACE", "ADD", "REMOVE"];
     var t = fTypes.indexOf(type.toUpperCase());
 
+    //saveView();
+
+
     switch(t){
         case 0:
-            sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.ALL);
+            type = tableau.FilterUpdateType.ALL;
             break;
         case 1:
-            sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.REPLACE);
+            type = tableau.FilterUpdateType.REPLACE;
             break;
         case 2:
-            sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.ADD);
+            type = tableau.FilterUpdateType.ADD;
             break;
         case 3:
-            sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.REMOVE);
+            type = tableau.FilterUpdateType.REMOVE;
             break;
         default:
             console.log("invalid filter type");
     }
 
+    sheet.applyFilterAsync(field, filter, type).then(function(){
+        refreshData();
+    })
+
+
+
+    // switch(t){
+    //     case 0:
+    //         sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.ALL);
+    //         break;
+    //     case 1:
+    //         sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.REPLACE);
+    //         break;
+    //     case 2:
+    //         sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.ADD);
+    //         break;
+    //     case 3:
+    //         sheet.applyFilterAsync(field, filter, tableau.FilterUpdateType.REMOVE);
+    //         break;
+    //     default:
+    //         console.log("invalid filter type");
+    // }
+
+}
+
+//----------------------------------------------------------------
+function refreshData(){
+    viz.refreshDataAsync();
+}
+
+
+
+//pauseAutomaticUpdatesAsync()
+//----------------------------------------------------------------
+function getMarks(){
+    activeSheet.getSelectedMarksAsync().then(function(marks){
+        pairs = marks[0].getPairs();
+        for(i in pairs){
+            console.log("pair ", i, ": ", pairs[i]);
+        }
+        //console.log("Selected Marks:", marks);
+    });
+}
+//----------------------------------------------------------------
+function saveView(){
+    console.log("log saveView()");
+    cv = "savedView";
+    workbook = viz.getWorkbook();
+    console.log(workbook);
+    //cv = "view: " + views.length.toString();
+    try{
+        console.log("log saveView()  2");
+        workbook.rememberCustomViewAsync("savedView").then(function(){
+            console.log("Saved Custom View");
+            views.push(cv);
+        },function(err){
+            console.log("err code: ", err.tableauSoftwareErrorCode);
+            console.log("err message: ", err.message);
+
+        });
+    }
+    catch(err){
+        console.log("err code: ", err.tableauSoftwareErrorCode);
+        console.log("err message: ", err.message);
+    }
+    
+}
+
+function getFilters(){
+
+    var sheet = activeSheet;
+
+    if(sheet.getSheetType() === "dashboard"){
+        
+        sheets = sheet.getWorksheets();
+        slen = sheets.length;
+        console.log("dashboard size: ", sheets.length);
+        for(i = 0; i < slen; i++){
+            console.log("sheet in dashboard: ", i);
+            getWorksheetFilters(sheets[i]);
+        }
+    }
+    else{
+        //console.log("not dashboard: ", sheet.getSheetType());
+        getWorksheetFilters(sheet);
+    }
+
+
+}
+
+function getWorksheetFilters(sheet){
+    console.log("sheet: ", sheet);
+    console.log("sheet name: ", sheet.getName());
+    console.log("sheet type: ", sheet.getSheetType());
+    sheet.getFiltersAsync().then(function(filters){
+        console.log(filters.length, "filters applied to ", sheet.getName(), ":", filters);
+
+        for(i in filters){
+            console.log("filter: ", i, ", ", filter[i].getName(), filter[i]);
+        }
+    });
+
+}
+
+function undo(){
+
+    if(views.length > 0){
+        last = views.pop();
+    }
+    workbook.showCustomViewAsync(last).then(function(cv){
+        workbook.removeCustomViewAsync(cv.getName()).then(function(cv){
+
+        });
+
+    });
+
+}
+
+function alertViewChange(){
+    console.log("view changed");
 }
 
 //----------------------------------------------------------------
